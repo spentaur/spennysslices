88eeba7e450e1cd25dde566f3cc2fc49cc280d34
Spencer Adams <hi@spentaur.com>
2023-01-21 19:50:37 +0000

    initial styling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,7 @@
 export default function Index() {
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline">Hello worlds!</h1>
+    <div>
+      <img src="spennys-slices-logo.svg" alt="logo" />
+    </div>
   );
 }
